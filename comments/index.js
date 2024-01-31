330fee67ffc7c13c1680846b6fbfb339a89b4bf0
Sazad Ahemad <mail2sazad@gmail.com>
2024-02-01 00:40:11 +0530

    Added Services for Posts & Comments

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+const port = 4001;
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const cid = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: cid, content });
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(port, () => {
+    console.log('Comments Service Listening on '+port);
+})
